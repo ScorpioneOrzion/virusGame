13aed8d02befc350dee54aea39c8e2ce2d294406
tijmentij <36512390+tijmentij@users.noreply.github.com>
2020-09-24 10:37:18 +0200

    made particle extend a general class

---
+++ movingObject.js
@@ -0,0 +1,34 @@
+export class movingObject {
+  constructor(widthField, heightField, speed) {
+    this.baseValues = [
+      widthField, heightField, speed
+    ]
+    this.time = 0
+    this.reset()
+  }
+
+  reset() {
+    this.x = movingObject.random(this.baseValues[0])
+    this.y = movingObject.random(this.baseValues[1])
+    this.direction = movingObject.normalize({ x: particle.random(10) - 5, y: particle.random(10) - 5 })
+    this.direction.x *= this.baseValues[2]
+    this.direction.y *= this.baseValues[2]
+  }
+
+  update(time) {
+    this.x += this.direction.x * time
+    this.y += this.direction.y * time
+    this.time += time
+  }
+
+  static random(x) {
+    return Math.floor(Math.random() * x)
+  }
+
+  static normalize(vector) {
+    let length = (vector.x ** 2 + vector.y ** 2) ** (1 / 2)
+    vector.x /= length
+    vector.y /= length
+    return vector
+  }
+}
--- particle.js
+++ particle.js
@@ -1,38 +1,24 @@
-export class particle {
+import { movingObject } from './movingObject'
+
+export class particle extends movingObject {
   constructor(listStates, widthField, heightField, speed, maxTime) {
-    this.baseValues = [listStates, widthField, heightField, speed]
+    super(widthField, heightField, speed)
+    this.baseValues.push(listStates)
     this.maxTime = maxTime
     this.reset()
-    this.time = particle.random(this.maxTime)
+    this.time = movingObject.random(this.maxTime)
   }
 
   reset() {
-    this.state = this.baseValues[0][0]
-    this.x = particle.random(this.baseValues[1])
-    this.y = particle.random(this.baseValues[2])
-    this.direction = particle.normalize({ x: particle.random(10) - 5, y: particle.random(10) - 5 })
-    this.direction.x *= this.baseValues[3]
-    this.direction.y *= this.baseValues[3]
+    super.reset()
+    this.state = this.baseValues[3][0]
   }
 
   update(time) {
-    this.x += this.direction.x * time
-    this.y += this.direction.y * time
-    this.time += time
+    super.update(time)
     if (this.time >= this.maxTime) {
       this.reset()
       this.time %= this.maxTime
     }
-  }
-
-  static random(x) {
-    return Math.floor(Math.random() * x)
-  }
-
-  static normalize(vector) {
-    let length = (vector.x ** 2 + vector.y ** 2) ** (1 / 2)
-    vector.x /= length
-    vector.y /= length
-    return vector
   }
 }
